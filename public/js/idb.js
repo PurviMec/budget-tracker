f8e7b7222a68c9fb5610ba031ccf99b7ba7d0615
PurviMec <princy.mecwan94@gmail.com>
2022-01-06 11:44:16 -0500

    create function to store obj data to indexedDB

---
+++ public/js/idb.js
@@ -0,0 +1,30 @@
+let db;
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function(event) {
+
+    const db = event.target.result;
+ 
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        //uploadBudget();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Error: " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+
+    const store = transaction.objectStore("new_budget");
+
+    store.add(record);
+}
